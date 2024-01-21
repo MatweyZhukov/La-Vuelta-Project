bd788301d2e49269fd0fab4bad36e1f1259986f4
Zhukov Matwey <106314713+MatweyZhukov@users.noreply.github.com>
2024-01-21 16:13:56 +0300

    Delete hooks directory

--- hooks/useAppDispatch.ts
+++
@@ -1,7 +0,0 @@
-//Global
-import { useDispatch } from "react-redux";
-
-//Types
-import { AppDispatch } from "@/GlobalRedux/store";
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
--- hooks/useTyppedSelector.ts
+++
@@ -1,7 +0,0 @@
-//Global
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-
-//Types
-import { RootState } from "@/GlobalRedux/store";
-
-export const useTyppedSelector: TypedUseSelectorHook<RootState> = useSelector;
